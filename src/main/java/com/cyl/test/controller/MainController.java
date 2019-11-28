7d06d5795a74fbb3eb64e7cc7d3c6251cb2d05e9
youlingman <chenyoucyl@gmail.com>
2019-11-28 10:20:47 +0800

    thymeleaf demo

--- src/main/java/com/cyl/test/controller/MainController.java
+++ src/main/java/com/cyl/test/controller/MainController.java
@@ -5,9 +5,11 @@ import com.alibaba.fastjson.JSONObject;
 import com.cyl.test.entity.Car;
 import com.cyl.test.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
@@ -20,7 +22,7 @@ import java.util.ArrayList;
 import java.util.Map;
 
 /**
-    处理http请求基本场景
+ * 处理http请求基本场景
  */
 @Controller
 @RequestMapping("/")
@@ -32,7 +34,7 @@ public class MainController {
     @PostMapping("/json")
     @ResponseBody
     public JSONObject postBodyHandler(@RequestBody User user) {
-        JSONObject jo = (JSONObject)JSON.toJSON(user);
+        JSONObject jo = (JSONObject) JSON.toJSON(user);
         JSONObject res = new JSONObject();
         res.put("user", jo);
         System.out.println(res.toJSONString());
@@ -46,10 +48,13 @@ public class MainController {
     // support form data/urlencoded content type
     @RequestMapping("/params")
     @ResponseBody
-    public Object requestParamsMapHandler(@RequestParam /*Map*/MultiValueMap<String, String> params) {
+    public Object requestParamsMapHandler(@RequestParam /*Map*/MultiValueMap<String, String> params, Model model) {
         System.out.println(params.toString());
+        System.out.println(model.toString());
 //        params.put("modified", "params");
-        params.put("modified", new ArrayList<String>(){{ add("params"); }});
+        params.put("modified", new ArrayList<String>() {{
+            add("params");
+        }});
         return params;
     }
 
@@ -62,8 +67,8 @@ public class MainController {
     @RequestMapping("/params-obj")
     @ResponseBody
     public Object requestParamsObjectHandler(User user, Car car, HttpServletRequest request) {
-        JSONObject ujo = (JSONObject)JSON.toJSON(user);
-        JSONObject cjo = (JSONObject)JSON.toJSON(car);
+        JSONObject ujo = (JSONObject) JSON.toJSON(user);
+        JSONObject cjo = (JSONObject) JSON.toJSON(car);
         JSONObject res = new JSONObject();
         res.put("user", ujo);
         res.put("car", cjo);
@@ -75,36 +80,48 @@ public class MainController {
 
     // client side redirect
     // return 301/302 http response
-    // todo how to redirect with origin params?
     @RequestMapping("/redirect")
     @ResponseBody
-    public String redirectHandler(@RequestParam Map params, HttpServletResponse response) throws IOException {
+    public String redirectHandler(@RequestParam Map<String, String> params, HttpServletResponse response, RedirectAttributes redirectAttributes) throws IOException {
         System.out.println(params.toString());
-        if(params.containsKey("action") && "redirect".equals(params.get("action"))) {
-            response.sendRedirect("params");
-            return null;
-        }
+        // not work for sendRedirect to HttpServletResponse
+        redirectAttributes.addAllAttributes(params);
+        redirectAttributes.addFlashAttribute("message", "redirect from /redirect");
+        response.sendRedirect("params");
         // return value would be drop for Redirect in HttpServletResponse
         // update: redirect setting is prior to content response
         return null;
     }
 
+    @RequestMapping("/redirect2")
+    public String redirect2Handler(@RequestParam Map params, RedirectAttributes redirectAttributes) throws IOException {
+        System.out.println(params.toString());
+        // work in resolve redirect view
+        redirectAttributes.addAllAttributes(params);
+        redirectAttributes.addFlashAttribute("message", "redirect from /redirect");
+        // string view name resolve
+        return "redirect:/params";
+    }
+
     // server side forward
-    // todo how to share model/session between controller/method?
     @RequestMapping("/forward")
     @ResponseBody
     public String forwardHandler(@RequestParam Map params, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println(params.toString());
-        if(params.containsKey("action") && "forward".equals(params.get("action"))) {
-//            response.sendRedirect("params");
-            System.out.println("enter forward");
-            // support relative or absolute path
-            request.getRequestDispatcher("params").forward(request, response);
-            return null;
-        }
+        System.out.println("enter forward");
+        // support relative or absolute path
+        request.getRequestDispatcher("params").forward(request, response);
         // return value would be drop for setting forward path in RequestDispatcher
         // update: RequestDispatcher
         return null;
+    }
+
+    @RequestMapping("/forward2")
+    public String forward2Handler(@RequestParam Map params, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println(params.toString());
+        System.out.println("enter forward2");
+        // string view name resolve
+        return "forward:/params";
     }
 
     // handle headers
@@ -130,10 +147,10 @@ public class MainController {
     // the only way to set cookie to response is bind out the HttpServletResponse in method parameter
     @RequestMapping("/cookie")
     @ResponseBody
-    public Object cookieHandler(@CookieValue Cookie session, HttpServletRequest request,  HttpServletResponse response) throws ServletException, IOException {
+    public Object cookieHandler(@CookieValue Cookie session, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println(JSONObject.toJSONString(session));
         Cookie[] cookies = request.getCookies();
-        for(Cookie cookie : cookies) {
+        for (Cookie cookie : cookies) {
             System.out.println(JSONObject.toJSONString(cookie));
         }
         Cookie resCookie = new Cookie("token", "123456");
---
+++ src/main/java/com/cyl/test/controller/ThymeleafPageController.java
@@ -0,0 +1,42 @@
+package com.cyl.test.controller;
+
+import com.cyl.test.entity.User;
+import com.cyl.test.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/thymeleaf")
+public class ThymeleafPageController {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping
+    public String get(@ModelAttribute("user") User user) {
+        System.out.println(user.toString());
+        return "index";
+    }
+
+    @PostMapping
+    public String postUser(@ModelAttribute("user") User user, RedirectAttributes redirectAttributes) {
+        System.out.println(user.toString());
+        try {
+            userMapper.insert(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+            // todo show some error message
+            redirectAttributes.addFlashAttribute("message", e.getMessage());
+            return "redirect:/thymeleaf";
+        }
+        // todo show some success message, share model attribute with redirect
+        redirectAttributes.addFlashAttribute("message", "success add user " + user.getName() + "!");
+        return "redirect:/thymeleaf";
+    }
+}
