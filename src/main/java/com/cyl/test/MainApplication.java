bcb8fabe493924c0da20274fa9b7d529be14c46a
youlingman <chenyoucyl@gmail.com>
2019-12-17 17:15:57 +0800

    add war build support

--- src/main/java/com/cyl/test/MainApplication.java
+++ src/main/java/com/cyl/test/MainApplication.java
@@ -6,11 +6,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.cyl.test.repository")
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MainApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
